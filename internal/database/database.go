2ca39966951aefa4e416dd7a9de85ad92e173357
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-02-25 14:56:16 +0300

    Database implementation started.

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,44 @@
 package database
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
+type Client struct {
+	databasePath string
+}
+type databaseSchema struct {
+	Users map[string]User `json:"users"`
+	Posts map[string]Post `json:"posts"`
+}
+
+func NewClient(databasePath string) Client {
+	return Client{
+		databasePath: databasePath,
+	}
+}
+
+func createDatabase(c Client) error {
+	dat, err := json.Marshal(databaseSchema{
+		Users: make(map[string]User),
+		Posts: make(map[string]Post),
+	})
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(c.databasePath, dat, 0600)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func ensureDatabase(c Client) error {
+	_, err := os.ReadFile(c.databasePath)
+	if errors.Is(err, os.ErrNotExist) {
+		return createDatabase(c)
+	}
+	return err
+}
---
+++ internal/database/user.go
@@ -0,0 +1,13 @@
+package database
+
+import (
+	"time"
+)
+
+type User struct {
+	CreatedAt time.Time `json:"createdAt"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Name      string    `json:"name"`
+	Age       int       `json:"age"`
+}
