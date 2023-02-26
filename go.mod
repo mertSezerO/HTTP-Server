ef8e679386d2a62c5a4af1debad513356b34e73a
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-02-26 21:14:19 +0300

    CRUD implementations made for Posts.

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
-module http-server
+module github.com/mertSezerO/HTTP-Server
 
 go 1.20
+
+require github.com/google/uuid v1.3.0 // indirect
+
--- internal/database/post.go
+++ internal/database/post.go
@@ -1,7 +1,10 @@
 package database
 
 import (
+	"errors"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Post struct {
@@ -9,4 +12,59 @@ type Post struct {
 	CreatedAt time.Time `json:"createdAt"`
 	UserEmail string    `json:"userEmail"`
 	Text      string    `json:"text"`
+}
+
+func (c Client) CreatePost(userEmail, text string) (Post, error) {
+	db, err := c.readDatabase()
+	if err != nil {
+		return Post{}, err
+	}
+	if _, exist := db.Users[userEmail]; !exist {
+		return Post{}, errors.New("the user does not exists")
+	}
+	post := Post{
+		ID:        uuid.New().String(),
+		CreatedAt: time.Now(),
+		UserEmail: userEmail,
+		Text:      text,
+	}
+	db.Posts[post.ID] = post
+	err = c.updateDatabase(db)
+	if err != nil {
+		return Post{}, err
+	}
+	return post, nil
+}
+
+func (c Client) GetPosts(userEmail string) ([]Post, error) {
+	db, err := c.readDatabase()
+	if err != nil {
+		return []Post{}, err
+	}
+	if _, exist := db.Users[userEmail]; !exist {
+		return []Post{}, errors.New("the user does not exists")
+	}
+	posts := []Post{}
+	for _, post := range db.Posts {
+		if post.UserEmail == userEmail {
+			posts = append(posts, post)
+		}
+	}
+	return posts, nil
+}
+
+func (c Client) DeletePost(id string) error {
+	db, err := c.readDatabase()
+	if err != nil {
+		return err
+	}
+	if _, exist := db.Posts[id]; !exist {
+		return errors.New("the post does not exists")
+	}
+	delete(db.Posts, id)
+	err = c.updateDatabase(db)
+	if err != nil {
+		return err
+	}
+	return nil
 }
